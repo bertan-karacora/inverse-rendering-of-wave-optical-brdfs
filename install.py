fcd56658033547268fd0d1d411d0d914595a302a
Bertan Karacora <bertan.karacora@gmail.com>
2021-12-16 21:08:49 +0100

    Remove installation helper

--- install.py
+++
@@ -1,7 +0,0 @@
-from subprocess import check_call, CalledProcessError
-import os
-
-try:
-    check_call(["apt", "install", "libeigen3-dev", "libopenexr-dev"], stdout=open(os.devnull,"wb"))
-except CalledProcessError as e:
-    print(e.output)
