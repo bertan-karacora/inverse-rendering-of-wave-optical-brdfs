b8bcc600d3374bc8e55eff6f322a9ea3386694ec
Bertan Karacora <bertan.karacora@gmail.com>
2022-01-21 01:29:19 +0100

    Wip

--- main.py
+++ main.py
@@ -8,22 +8,14 @@ from WaveOpticsBrdfWithPybind.brdf import init, makeQuery, Query, BrdfImage, Brd
 init()
 
 refImage = EXRImage(args.reference)
-# EXRImage.writeImage(refImage.values, args.resolution, args.resolution, args.save_path)
-
 refHeightfield = Heightfield(refImage.values, refImage.width, refImage.height, args.texel_width, args.vert_scale)
-# EXRImage.writeImage(refHeightfield.values, args.resolution, args.resolution, args.save_path)
-
-print(refHeightfield.getValue(40, 50))
 
 gaborBasis = refHeightfield.toGaborBasis()
 hf = Heightfield(gaborBasis, refImage.width, refImage.height, args.texel_width, args.vert_scale)
-EXRImage.writeImage(hf.values, args.resolution, args.resolution, args.save_path)
+EXRImage.writeImage(np.absolute(hf.values - refHeightfield.values), args.resolution, args.resolution, "Results/GabortransformResiduum.exr")
 
-print(hf.getValue(40, 50))
+query = makeQuery(args.x, args.y, args.sigma, args.lambda_, args.light_x, args.light_y)
 
-# query = makeQuery(args.x, args.y, args.sigma, args.lambda_, args.light_x, args.light_y)
-
-# brdf = WaveBrdfAccel(args.diff_model, gaborBasis, refHeightfield.width, refHeightfield.height, refHeightfield.texelWidth)
-# result = brdf.genBrdfImage(query, args.resolution)
-
-# EXRImage.writeImageRGB(result.r, result.g, result.b, args.resolution, args.resolution, args.save_path)
+brdfFunction = WaveBrdfAccel(args.diff_model, gaborBasis, refHeightfield.width, refHeightfield.height, refHeightfield.texelWidth)
+result = brdfFunction.genBrdfImage(query, args.resolution)
+EXRImage.writeImageRGB(result.r, result.g, result.b, args.resolution, args.resolution, "Results/Brdf.exr")
